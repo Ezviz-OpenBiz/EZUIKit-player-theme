d3df74f4d2ee776f92b0f35c4e61c91012e99545
ShineShao <xiaoshaoqq@gmail.com>
2026-02-09 14:06:01 +0800

    fix: min height progress slider show

--- dist/style.js
+++ dist/style.js
@@ -1,6 +1,6 @@
 /*
- * @ezuikit/player-theme v2.1.1-beta.1
- * Copyright (c) 2026-02-05 Ezviz-OpenBiz
- * Released under the MIT License.
- */
+* @ezuikit/player-theme v2.1.2-beta.1
+* Copyright (c) 2026-02-09 Ezviz-OpenBiz
+* Released under the MIT License.
+*/
 require("./style.css");
