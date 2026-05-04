406ad468db5135f2cd2930822c3ea08eacbd75e0
ShineShao <xiaoshaoqq@gmail.com>
2026-05-04 17:13:13 +0800

    feat: add vue2.5.17 demo

---
+++ examples/with-vue2.5.17/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <TimeLine />
+    <Theme />
+  </div>
+</template>
+
+<script>
+import TimeLine from './components/index.vue';
+import Theme from './components/theme.vue';
+
+export default {
+  name: 'app',
+  components: {
+    TimeLine,
+    Theme
+  },
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+  height: 150vh;
+}
+</style>
---
+++ examples/with-vue2.5.17/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['vue-style-loader', 'css-loader']
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  },
+  resolve: {
+    alias: {
+      'vue$': 'vue/dist/vue.esm.js'
+    }
+  },
+  performance: {
+    hints: false
+  },
+  devtool: '#eval-source-map'
+}
