50612ecfd0491e5b188e38e081350ca515512f40
ShineShao <xiaoshaoqq@gmail.com>
2026-05-04 19:17:52 +0800

    feat: update demo

---
+++ examples/with-vue2.5/src/components/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div style="padding-top: 100px">
+    <div id="mobile-time-line"></div>
+  </div>
+</template>
+
+<script>
+import '@ezuikit/control-time-line/dist/style/style.css';
+import { MobileTimeLine } from '@ezuikit/control-time-line';
+
+let mobileTimeLine;
+
+export default {
+  name: 'TimeLine',
+  mounted: () => {
+    if (mobileTimeLine) {
+      mobileTimeLine.destroy();
+      mobileTimeLine = null;
+    }
+
+    mobileTimeLine = new MobileTimeLine(document.getElementById('mobile-time-line'), {
+      height: 350,
+      width: '100%',
+      current: new Date(),
+      // timeSections: DATA,
+      currentTimeBgColor: 'red',
+      currentTimeColor: '#ffffff',
+      language: 'zh',
+      readOnly: false,
+      showTimeWidthBtn: true,
+      timeAxisBgColor: '#bdcdff',
+      timePointColor: '#648ffc',
+      timeSectionColor: '#369fff',
+      timeTextColor: '#666666',
+      timeWidth: 0,
+      width: '100%',
+      timeWidth: 0,
+      language: 'zh',
+      onPickerOpenChange: function (open) {
+        console.log('onPickerOpenChange', open);
+      },
+      onPickerSelect: function (item) {
+        console.log('onPickerSelect', item);
+      },
+      onChange: function (current) {
+        console.warn('onChange', current);
+        // timeLine.update(current.getTime());
+      },
+      onDragStart: function (current) {
+        console.log('onDragStart', current);
+      },
+      onDragging: function (current) {
+        console.log('onDragging', current);
+      },
+      onDragEnd: function (current) {
+        console.log('onDragEnd', current);
+      },
+    });
+
+    // mobileTimeLine.updateTimeSections(DATA);
+  },
+  methods: {},
+};
+</script>
